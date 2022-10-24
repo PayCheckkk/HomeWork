63046642782ce61d91576c10b4a968e5345827e0
PayCheck <paycheckkk@mail.ru>
2022-10-25 00:23:54 +0700

    1

--- Homework/Assets/Game/Coin.cs
+++ Homework/Assets/Game/Coin.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class Coin : MonoBehaviour
 {
     private SpriteRenderer _renderer;
